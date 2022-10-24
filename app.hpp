62b5671cba47ed3d45965996d885eb5e079462b9
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2022-10-24 15:16:23 +0800

    add UTF8

---
+++ app.hpp
@@ -0,0 +1,10 @@
+
+#ifndef LIBTEST__APP_HPP_
+#define LIBTEST__APP_HPP_
+
+#include "viewmodel/viewmodel.hpp"
+#include "window/window.hpp"
+
+void init(Viewmodel *pVM,Window *pWin);
+
+#endif //LIBTEST__APP_HPP_
