01c7b14f042b653b6b2e8b75ef4bbcc1790bc40e
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2024-04-07 16:05:46 +0800

    use type() to simplify test code

--- Lib_VBao/python/test/user_derive_case_test.py
+++ Lib_VBao/python/test/user_derive_case_test.py
@@ -16,7 +16,7 @@ class WithArgClass:
         super().__init__(*args, **kwargs)
 
 
-class OverlapClass:
+class CollideClass:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -27,105 +27,132 @@ class OverlapClass:
         pass
 
 
-class TestVBAOUserDerive:
-    class ModelDerive_MROLeft(vbao.Model, NoArgClass):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+base = {'Model': vbao.Model, 'ViewModel': vbao.ViewModel, 'View': vbao.View}
+user_class = {'NoArg': NoArgClass, 'WithArg': WithArgClass, 'Collide': CollideClass}
+mro_suffix = {True: '_MROLeft', False: '_MRORight'}
 
-    class ModelDerive_MRORight(NoArgClass, vbao.Model):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
 
-    class ViewDerive_MROLeft(vbao.View, NoArgClass):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
+def makeClass(vbao_base: str, other: str, mro_left=True):
+    name = ''.join([vbao_base, 'Derive', other, mro_suffix[mro_left]])
+    if mro_left:
+        return type(name, (base[vbao_base], user_class[other]), {})
+    else:
+        return type(name, (user_class[other], base[vbao_base]), {})
 
-    class ViewDerive_MRORight(NoArgClass, vbao.View):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
 
-    class ViewModelDeriveArg_MROLeft(vbao.ViewModel, WithArgClass):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-    class ViewModelDeriveArg_MRORight(WithArgClass, vbao.ViewModel):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-    class ViewModelDeriveConflict_MROLeft(vbao.ViewModel, OverlapClass):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-    class ViewModelDeriveConflict_MRORight(OverlapClass, vbao.ViewModel):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
+class TestBasicDerive:
     # Derive1 -- View/Model (...) -- NoArgsClass -- object
     def test_model_derive_no_arg_succeed(self):
-        self.ModelDerive_MROLeft()
+        cls = makeClass('Model', 'NoArg')
+        assert cls.__name__ == 'ModelDeriveNoArg_MROLeft'
+        cls()
 
     def test_model_derive_no_arg_fail(self):
+        cls = makeClass('Model', 'NoArg')
         with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
-            self.ModelDerive_MROLeft(no_use=1)
+            cls(no_use=1)
+
+    def test_viewmodel_derive_no_arg_succeed(self):
+        cls = makeClass('ViewModel', 'NoArg')
+        assert cls.__name__ == 'ViewModelDeriveNoArg_MROLeft'
+        cls()
+
+    def test_viewmodel_derive_no_arg_fail(self):
+        cls = makeClass('ViewModel', 'NoArg')
+        with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
+            cls(no_use=1)
 
     def test_view_derive_no_arg_succeed(self):
-        self.ViewDerive_MROLeft()
+        cls = makeClass('View', 'NoArg')
+        assert cls.__name__ == 'ViewDeriveNoArg_MROLeft'
+        cls()
 
     def test_view_derive_no_arg_fail(self):
+        cls = makeClass('View', 'NoArg')
         with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
-            self.ViewDerive_MROLeft(no_use=1)
+            cls(no_use=1)
 
     # Derive2 -- NoArgsClass -- View/Model (...) -- object
     def test_model_derive_no_arg_succeed_diff_mro(self):
-        self.ModelDerive_MRORight()
+        cls = makeClass('Model', 'NoArg', False)
+        assert cls.__name__ == 'ModelDeriveNoArg_MRORight'
+        cls()
 
     def test_model_derive_no_arg_fail_diff_mro(self):
+        cls = makeClass('Model', 'NoArg', False)
         with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
-            self.ModelDerive_MRORight(no_use=1)
+            cls(no_use=1)
+
+    def test_viewmodel_derive_no_arg_succeed_diff_mro(self):
+        cls = makeClass('ViewModel', 'NoArg', False)
+        assert cls.__name__ == 'ViewModelDeriveNoArg_MRORight'
+        cls()
+
+    def test_viewmodel_derive_no_arg_fail_diff_mro(self):
+        cls = makeClass('ViewModel', 'NoArg', False)
+        with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
+            cls(no_use=1)
 
     def test_view_derive_no_arg_succeed_diff_mro(self):
-        self.ViewDerive_MRORight()
+        cls = makeClass('View', 'NoArg', False)
+        assert cls.__name__ == 'ViewDeriveNoArg_MRORight'
+        cls()
 
     def test_view_derive_no_arg_fail_diff_mro(self):
+        cls = makeClass('View', 'NoArg', False)
         with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
-            self.ViewDerive_MRORight(no_use=1)
+            cls(no_use=1)
 
+
+class TestVBAOUserDerive:
     # Derive3 -- ViewModel (...) -- WithArgsClass -- object
-    def test_viewmodel_derive_with_arg_succeed(self):
-        self.ViewModelDeriveArg_MROLeft(True)
-
-    def test_viewmodel_derive_with_arg_fail(self):
+    def test_viewmodel_derive_with_arg(self):
+        cls = makeClass('ViewModel', 'WithArg')
+        assert cls.__name__ == 'ViewModelDeriveWithArg_MROLeft'
+        # success
+        cls(True)
+        # error
         with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
-            self.ViewModelDeriveArg_MROLeft(verbose=True, no_use=1)
+            cls(verbose=True, no_use=1)
 
     # Derive4 -- WithArgsClass -- ViewModel (...) -- object
-    def test_viewmodel_derive_with_arg_succeed_diff_mro(self):
-        self.ViewModelDeriveArg_MRORight(True)
-
-    def test_viewmodel_derive_with_arg_fail_diff_mro(self):
+    def test_viewmodel_derive_with_arg_diff_mro(self):
+        cls = makeClass('ViewModel', 'WithArg', False)
+        assert cls.__name__ == 'ViewModelDeriveWithArg_MRORight'
+        # success
+        cls(True)
+        # error
         with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
-            self.ViewModelDeriveArg_MRORight(verbose=True, no_use=1)
+            cls(verbose=True, no_use=1)
 
     # Derive5 -- ViewModel (...) -- OverlapClass -- object
     # ps: View's property is not bound
-    def test_view_derive_conflict_ctor_succeed(self):
-        self.ViewModelDeriveConflict_MROLeft()
-
-    def test_view_derive_conflict_ctor_fail(self):
+    def test_viewmodel_derive_conflict_ctor(self):
+        cls = makeClass('ViewModel', 'Collide')
+        assert cls.__name__ == 'ViewModelDeriveCollide_MROLeft'
+        # success
+        cls()
+        # error
         with pytest.raises(TypeError, match=r"object\.__init__\(\) takes exactly one argument"):
-            self.ViewModelDeriveConflict_MROLeft(no_use=1)
+            cls(verbose=True, no_use=1)
 
-    def test_view_derive_conflict_set_property_succeed(self):
-        v = self.ViewModelDeriveConflict_MROLeft()
+    mixin_enabled = True
+
+    @pytest.mark.skipif(not mixin_enabled, reason='')
+    def test_viewmodel_derive_conflict_set_property(self):
+        v = makeClass('ViewModel', 'Collide')()
         assert '1' not in v.properties
         v.setProperty('1', 2)
         assert '1' in v.properties
 
-    def test_view_derive_conflict_get_property_succeed(self):
-        v = self.ViewModelDeriveConflict_MROLeft()
+    @pytest.mark.skipif(not mixin_enabled, reason='')
+    def test_viewmodel_derive_conflict_get_property(self):
+        v = makeClass('ViewModel', 'Collide')()
         assert '1' not in v.properties
         v.setProperty('1', 2)
         assert v.getProperty('1') == 2
+
+    # TODO:处理在ViewModel右边时。需要获取是否命名避让
 
 
 if __name__ == '__main__':
